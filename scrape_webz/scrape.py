574d715921a0e70450cb15427451853da75425f5
Leon Markovich <leonmarkovich97@gmail.com>
2024-05-23 20:19:15 +0300

    scraped first page, without last post, as it has a quote

--- scrape_webz/scrape.py
+++ scrape_webz/scrape.py
@@ -1,16 +1,18 @@
 import re
 import requests
 res=requests.get('https://www.phpbb.com/community/viewtopic.php?f=46&t=2159437')
-con=res.text
-# print(con)
+con=res.text.replace('<br>','\n')
+
 post_pat = r'<div id="p\d+"$>(.*?)</div>'
 post_pat=r'<div id="p\d+" class="post has-profile bg\d">(.*?)(?=<div\s+id="sig\d+"\s+class="signature">)'
 #could have been splitted to 4 different groups, maybe fix later..
 posts = re.findall(post_pat, con, re.DOTALL) # get all posts exclude the signature
 title_ptn = r'<div id="post_content\d+">.*?<a href="[^"]*">(.*?)</a>'
-author_ptn = r'<div class="avatar-container">.*?<a href="[^"]*">(.*?)</a>'
+# author_ptn = r'<a href="\.\/member.*? class="username">(.*?)</a>'
+author_ptn = r'<a.*?class="username.*?">(.*?)</a>'
 date_ptn = r'<time datetime="([^"]*)">'
 content_ptn=r'<div class="content">(.*?)</div>'
+
 
 
 #works, but includes br tags and img tags.. 
@@ -22,4 +24,5 @@ for p in posts:
     date = re.search(date_ptn, p, re.DOTALL)
     print(f'date={date.group(1)}\n')
     content = re.search(content_ptn, p, re.DOTALL)
-    print(f'content={content.group(1)}\n')
+    # print(f'content={content.group(1)}\n')
+    print(re.sub(r'<[^>]*>', '', content.group(1)))
