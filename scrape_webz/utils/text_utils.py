9f7d262aeeec39b71df669dfa84c290f116692d9
Leon Markovich <leonmarkovich97@gmail.com>
2024-05-24 15:43:39 +0300

    finished code removed tests

--- scrape_webz/utils/text_utils.py
+++ scrape_webz/utils/text_utils.py
@@ -1,7 +1,6 @@
 import re
 def clean_text(text):
         '''Clean the text content by removing unwanted characters and HTML entities'''
-        # text = re.sub(r'[\t\r\n]', ' ', text)  # Replace tabs, carriage returns, and newlines with spaces
         text = re.sub(r'&quote;', '"', text)   # Replace HTML entity &quote; with a double quote
         text = re.sub(r'&.*?;', '', text)      # Remove any remaining HTML entities
         text = re.sub(r'<[^>]*>', '', text)    # Remove any HTML tags
